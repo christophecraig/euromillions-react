69133606e73597bf6b4b342ec060c7a612a851b0
Christophe Craig <christophe.craig@viacesi.fr>
2018-12-26 23:49:39 +0100

    attempted to add react-router, might need to figure out another way

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { Results } from './Results/Results'
 import './index.css';
 
